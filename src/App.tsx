e72a560d53faef3a922d3a8bedc22d549d53a3cf
MiHii <hieupham08111@gmail.com>
2024-11-05 12:44:28 +0700

    add layout components and implement routing for home, contact, and book pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from './pages/login';
+import ContactPage from './pages/contact';
+import BookPage from './pages/book';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 const Layout = () => {
-  return <>Home page</>;
+  return (
+    <div className='layout-app'>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-]);
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Layout />,
-    },
-    {
-      path: '/login',
-      element: <LoginPage />,
+      errorElement: <div>404 Not Found</div>,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: 'contact',
+          element: <ContactPage />,
+        },
+        {
+          path: 'book',
+          element: <BookPage />,
+        },
+      ],
     },
   ]);
   return (
